dafa94f2a99c5de8d97fdc329c4bbc4e6f7f04d1
levi-sr93 <levi-sr@outlook.com>
2021-08-31 10:47:38 -0300

    Alteraçao de estrutura | seperando elementos para dentro do SRC

--- App.js
+++ App.js
@@ -1,30 +1,7 @@
 import React from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+
+import {Home} from './src/pages/Home';
 
 export default function App() {
-  return (
-    <>
-      <View
-        style={{
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
-        <Text style={{fontSize: 22, fontWeight: 'bold'}}>Hello Universe !</Text>
-      </View>
-
-      <TouchableOpacity
-        style={{
-          alignSelf: 'center',
-          marginBottom: 30,
-          backgroundColor: 'teal',
-          borderRadius: 8,
-          padding: 20,
-        }}>
-        <Text style={{color: '#fff', fontSize: 20, fontWeight: 'bold'}}>
-          Start...
-        </Text>
-      </TouchableOpacity>
-    </>
-  );
+  return <Home />;
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {View, Text, TouchableOpacity} from 'react-native';
+
+export function Home() {
+  return (
+    <>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <Text style={{fontSize: 22, fontWeight: 'bold'}}>Hello Universe !</Text>
+      </View>
+
+      <TouchableOpacity
+        style={{
+          alignSelf: 'center',
+          marginBottom: 30,
+          backgroundColor: 'teal',
+          borderRadius: 8,
+          padding: 20,
+        }}>
+        <Text style={{color: '#fff', fontSize: 20, fontWeight: 'bold'}}>
+          Start...
+        </Text>
+      </TouchableOpacity>
+    </>
+  );
+}
