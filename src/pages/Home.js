c2b38eeab0ec23d485b5c473b9461435dab3ca18
levi-sr93 <levi-sr@outlook.com>
2021-08-31 11:19:07 -0300

    Utilizando StyleSheet | array de estilos para sobrescrever

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,30 +1,65 @@
 import React from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import {
+  SafeAreaView,
+  Text,
+  StyleSheet,
+  TextInput,
+  Platform,
+  TouchableOpacity,
+} from 'react-native';
 
 export function Home() {
   return (
     <>
-      <View
-        style={{
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
-        <Text style={{fontSize: 22, fontWeight: 'bold'}}>Hello Universe !</Text>
-      </View>
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.title}>Hello Universe !</Text>
 
-      <TouchableOpacity
-        style={{
-          alignSelf: 'center',
-          marginBottom: 30,
-          backgroundColor: 'teal',
-          borderRadius: 8,
-          padding: 20,
-        }}>
-        <Text style={{color: '#fff', fontSize: 20, fontWeight: 'bold'}}>
-          Start...
-        </Text>
-      </TouchableOpacity>
+        <TextInput
+          placeholder="new skill"
+          placeholderTextColor="#555"
+          style={styles.input}
+        />
+
+        <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+          <Text style={styles.buttonText}>Add</Text>
+        </TouchableOpacity>
+
+        <Text style={[styles.title, {marginTop: 50}]}>My Skills</Text>
+      </SafeAreaView>
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121015',
+    paddingHorizontal: 30,
+    paddingVertical: 50,
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+  input: {
+    backgroundColor: '#1f1e25',
+    color: '#fff',
+    fontSize: 18,
+    padding: Platform.OS === 'ios' ? 15 : 10,
+    marginTop: 30,
+    borderRadius: 5,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: '#a370f7',
+    padding: 15,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 17,
+    fontWeight: 'bold',
+  },
+});
